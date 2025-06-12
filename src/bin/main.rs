cdc4e7fba0f3b46592b697c257902eeeb4c34463
Liam Tietjens <l.tietjens@hotmail.com>
2025-06-12 15:09:37 +1200

    removed dfmt and rtt stuff

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,22 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
-use panic_rtt_target as _;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
 
 extern crate alloc;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     // generator version: 0.3.1
-
-    rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -24,8 +25,6 @@ async fn main(spawner: Spawner) {
 
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
-
-    info!("Embassy initialized!");
 
     let timer1 = TimerGroup::new(peripherals.TIMG0);
     let _init = esp_wifi::init(
@@ -39,7 +38,6 @@ async fn main(spawner: Spawner) {
     let _ = spawner;
 
     loop {
-        info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
 
