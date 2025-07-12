5dff4763a77158e4e3cee7ef061b3c9169caf132
Liam Tietjens <l.tietjens@hotmail.com>
2025-07-12 18:35:23 +1200

    status messages are working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,19 +6,25 @@ use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_config::{esp_config_int, esp_config_str};
 use esp_hal::clock::CpuClock;
-use esp_hal::rmt::Rmt;
-use esp_hal::time::Rate;
+
 use esp_hal::timer::{systimer::SystemTimer, timg::TimerGroup};
+use heapless::String;
+
 use log::info;
 
 extern crate alloc;
 
 use sonic_bt;
 
+#[allow(unused)]
 const WIFI_CHANNEL: u8 = esp_config_int!(u8, "WIFI_CHANNEL");
+#[allow(unused)]
 const WIFI_SSID: &str = esp_config_str!("WIFI_SSID");
+#[allow(unused)]
 const WIFI_PASSWORD: &str = esp_config_str!("WIFI_PASSWORD");
+#[allow(unused)]
 const MAX_PAYLOAD_SIZE: u16 = esp_config_int!(u16, "MAX_PAYLOAD_SIZE");
+#[allow(unused)]
 const STACK_SIZE: u16 = esp_config_int!(u16, "STACK_SIZE");
 
 #[esp_hal_embassy::main]
@@ -45,24 +51,30 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
-    // TODO: Spawn some tasks
-    let spawn = spawner;
+    spawner.spawn(sonic_bt::status::run()).unwrap();
 
-    spawner.spawn(sonic_bt::led::run()).unwrap();
-
-    sonic_bt::led::LED_STATUS.signal(sonic_bt::led::LEDStatus::Ok());
-
-    Timer::after(Duration::from_millis(500)).await;
-
-    sonic_bt::led::LED_STATUS.signal(sonic_bt::led::LEDStatus::Progressing());
+    sonic_bt::status::STATUS.signal(sonic_bt::status::Status::Ok(Some(
+        String::try_from("abc").unwrap(),
+    )));
 
     Timer::after(Duration::from_millis(500)).await;
 
-    sonic_bt::led::LED_STATUS.signal(sonic_bt::led::LEDStatus::Panic());
+    sonic_bt::status::STATUS.signal(sonic_bt::status::Status::Warn(None));
 
-    loop {
-        Timer::after(Duration::from_secs(1)).await;
-    }
+    Timer::after(Duration::from_millis(500)).await;
 
+    sonic_bt::status::STATUS.signal(sonic_bt::status::Status::Err(None));
+
+    Timer::after(Duration::from_millis(500)).await;
+
+    sonic_bt::status::STATUS.signal(sonic_bt::status::Status::Ok(Some(
+        String::try_from("abc11").unwrap(),
+    )));
+
+    Timer::after(Duration::from_millis(500)).await;
+
+    sonic_bt::status::STATUS.signal(sonic_bt::status::Status::Ok(Some(
+        String::try_from("abc22").unwrap(),
+    )));
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
